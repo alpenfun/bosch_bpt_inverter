ca2a193a7c60ae8e2cd4e74c0f60067e3090517a
alpenfun <maier.fm@gmail.com>
2025-02-07 13:44:34 +0100

    Update sensor.py

--- custom_components/bosch_bpt_inverter/sensor.py
+++ custom_components/bosch_bpt_inverter/sensor.py
@@ -8,7 +8,7 @@ die definierten Sensoren.
 Die Basis-URL wird in der configuration.yaml definiert, z. B.:
   
   bosch_bpt_inverter:
-    resource: "http://192.168.xxx.xxx"
+    resource: "http://192.168.11.46"
 
 Die Debug-Logeinträge werden nur ausgegeben, wenn DEBUG_LOGGING auf True gesetzt ist.
 """
